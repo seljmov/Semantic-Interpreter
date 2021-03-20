4543bc1cc8358ec5ddfca1348bd01310a4cd992e
Zagir Selimov <seljmov@list.ru>
2021-03-20 14:16:53 +0300

    Hello Wordl!

---
+++ Semantic-Interpreter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Semantic_Interpreter
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
