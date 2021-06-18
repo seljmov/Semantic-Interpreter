2858bd662197244142a70082503451702db78083
Zagir Selimov <seljmov@list.ru>
2021-06-18 17:53:22 +0400

    Добавлена интерполяция имени переменной

---
+++ Semantic-Interpreter/Core/Items/Expressions/InterpolationExpression.cs
@@ -0,0 +1,47 @@
+﻿using Semantic_Interpreter.Library;
+
+namespace Semantic_Interpreter.Core
+{
+    public class InterpolationExpression : IExpression
+    {
+        public InterpolationExpression(IExpression expression) => Expression = expression;
+        
+        private IExpression Expression { get; }
+        
+        public IValue Eval()
+        {
+            var text = Expression.Eval().AsString();
+            var result = "";
+            
+            for (var i = 0; i < text.Length; ++i)
+            {
+                if (text[i] != '$')
+                {
+                    result += text[i];
+                }
+                else
+                {
+                    ++i; // Skip $
+                    if (i < text.Length && text[i] == '{')
+                    {
+                        ++i; // Skip {
+                        // Узнаем имя переменной
+                        var name = "";
+                        while (i < text.Length && (char.IsLetterOrDigit(text[i]) || text[i] == '_'))
+                        {
+                            name += text[i];
+                            ++i;
+                        }
+
+                        ++i; // Skip }
+
+                        var value = VariablesStorage.At(name).Expression.Eval().AsString();
+                        result += value;
+                    }
+                }
+            }
+            
+            return new StringValue(result);
+        }
+    }
+}
